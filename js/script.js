11c7d9bc5735231c9755233355b59f175a2df298
puhrome <phirom.yim@fullsail.edu>
2014-04-11 20:34:28 -0400

    Expression_Worksheet1
    WebStorm HTML and JS of area of a rectangle

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//the area of a rectangle is width times height
+var width; //declare variable
+width = 4; //define width
+var height; //declare variable
+height = 5; //define height
+area = width * height;
+console.log(area);
